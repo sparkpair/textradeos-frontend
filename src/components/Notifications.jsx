31ab57785630c8a78acad4b61661a9cc721abe44
decorgraphicsstudio <hasanraza03165825495@gmail.com>
2025-11-08 14:17:58 +0500

    ..

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -1,3 +1,4 @@
+import { Bell } from "lucide-react";
 import React, { useState, useRef, useEffect } from "react";
 
 function Notifications() {
@@ -115,10 +116,10 @@ function Notifications() {
 
         {/* 🔹 Toggle Button */}
         <div
-          className="button w-4 h-16 bg-[#f8fbfb] drop-shadow-md border border-r-0 border-gray-300 absolute right-full top-40 rounded-l-md flex items-center justify-center cursor-pointer"
+          className="bg-[#127475] text-white hover:bg-[#0c5f60] active:scale-95 drop-shadow-md absolute right-full top-40 rounded-l-lg flex items-center justify-center cursor-pointer pl-1 py-3 pr-0.5"
           onClick={() => setIsNotificationOpen(!isNotificationOpen)}
         >
-          <svg
+          {/* <svg
             width="6"
             height="10"
             viewBox="0 0 5 10"
@@ -132,7 +133,8 @@ function Notifications() {
               d="M0.323625 4.17697L4.29881 0.133206C4.3959 0.0570885 4.45523 0 4.57389 0C4.8274 0 5 0.304472 5 0.732636C5 1.03711 4.89752 1.28449 4.70334 1.44624L1.13269 4.94767V5.0333L4.70334 8.54424C4.89752 8.71551 5 8.96289 5 9.26736C5 9.68601 4.8274 10 4.57389 10C4.44984 10 4.3959 9.94291 4.29881 9.86679L0.323625 5.81351C0.0916936 5.60419 0 5.3568 0 4.99524C0 4.63368 0.0916936 4.3863 0.323625 4.17697Z"
               fill="black"
             />
-          </svg>
+          </svg> */}
+          <Bell size={16} />
         </div>
       </div>
     </>
